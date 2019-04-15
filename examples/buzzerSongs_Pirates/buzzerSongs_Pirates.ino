0ca34511b79ab2c5e13c3513ba1255ab11cdf8a3
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-15 20:48:13 +0200

    Add files via upload

---
+++ examples/buzzerSongs_Pirates/buzzerSongs_Pirates.ino
@@ -0,0 +1,23 @@
+#include <ALKS.h>
+
+//Add requed files
+#include "pitches.h" 
+#include "themes.h"
+
+
+void setup(){
+}
+
+void loop(){
+  //For cycle which plays melod itself
+  for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
+
+        int noteDuration = 1000 / Pirates_duration[thisNote];
+        tone(piezo1, Pirates_note[thisNote], noteDuration);
+
+        int pauseBetweenNotes = noteDuration * 1.05; //Tempo. ou can set your own.
+        delay(pauseBetweenNotes);
+        noTone(piezo1); //Pause
+    }
+
+}
