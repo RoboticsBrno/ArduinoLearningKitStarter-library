b59b25f7b08e13ce6481acd8b9a76cab476fb699
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-15 19:27:08 +0200

    Update buzzerSongs_Pirates.ino

--- examples/ALKS_BuzzerSongs/buzzerSongs_Pirates.ino
+++ examples/ALKS_BuzzerSongs/buzzerSongs_Pirates.ino
@@ -21,11 +21,11 @@ void loop(){
   for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
 
         int noteDuration = 1000 / Pirates_duration[thisNote];
-        tone(_pin, Pirates_note[thisNote], noteDuration);
+        tone(bzucak, Pirates_note[thisNote], noteDuration);
 
         int pauseBetweenNotes = noteDuration * 1.05; //Tempo. Pokud chcete, můžete melodii zrychlit nebo zpomalit.
         delay(pauseBetweenNotes);
-        noTone(_pin); //Pauza
+        noTone(bzucak); //Pauza
     }
 
 }
