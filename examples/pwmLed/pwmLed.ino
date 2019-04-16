66f3d7f745814a3abf05fda71b6c1a1c2a2eed14
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-16 16:02:08 +0200

    Update pwmLed.ino

--- examples/pwmLed/pwmLed.ino
+++ examples/pwmLed/pwmLed.ino
@@ -8,10 +8,37 @@ int ledPin = 8;
 int potProm = 0;    
 int ledProm = 0;   
 
+const int freq = 5000; 
+const int ledChannel = 0; 
+const int resolution = 8;
+
+const int ledPin = 16;  // 16 corresponds to GPIO16
+
 void setup(){
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
 
 void loop(){
+  
+  #if defined(ESP8266) || defined(ESP32)
+     // increase the LED brightness
+      for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+        // changing the LED brightness with PWM
+        ledcWrite(ledChannel, dutyCycle);
+        delay(15);
+      }
+
+      // decrease the LED brightness
+      for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+        // changing the LED brightness with PWM
+        ledcWrite(ledChannel, dutyCycle);   
+        delay(15);
+      }
+    }
+  #endif
   
   potProm = analogRead(POT1);
 
