fdab4901d21eee19d1df81d338f4cebcc133c4d1
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-16 09:21:52 +0200

    Update buzzerSongs_Pirates.ino

--- examples/buzzerSongs_Pirates/buzzerSongs_Pirates.ino
+++ examples/buzzerSongs_Pirates/buzzerSongs_Pirates.ino
@@ -9,6 +9,20 @@ void setup(){
 }
 
 void loop(){
+  
+  //This code runs only on Esp 
+  #if defined(ESP8266) || defined(ESP32)
+  for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
+
+        int noteDuration = 1000 / Pirates_duration[thisNote];
+        tone(PIEZO1, Pirates_note[thisNote], noteDuration);
+
+        int pauseBetweenNotes = noteDuration * 1.05; //Tempo. ou can set your own.
+        delay(pauseBetweenNotes);
+        noTone(PIEZO1); //Pause
+    }
+  #endif
+ 
   //For cycle which plays melod itself
   for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
 
