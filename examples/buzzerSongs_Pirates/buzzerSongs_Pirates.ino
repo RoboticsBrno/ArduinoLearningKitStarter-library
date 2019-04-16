b78739607627d17700088af9e9ca174e8a32f2d7
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-16 09:23:04 +0200

    Update buzzerSongs_Pirates.ino

--- examples/buzzerSongs_Pirates/buzzerSongs_Pirates.ino
+++ examples/buzzerSongs_Pirates/buzzerSongs_Pirates.ino
@@ -15,7 +15,7 @@ void loop(){
   for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
 
         int noteDuration = 1000 / Pirates_duration[thisNote];
-        tone(PIEZO1, Pirates_note[thisNote], noteDuration);
+         ledcWriteTone(PIEZO1, Pirates_note[thisNote], noteDuration);
 
         int pauseBetweenNotes = noteDuration * 1.05; //Tempo. ou can set your own.
         delay(pauseBetweenNotes);
