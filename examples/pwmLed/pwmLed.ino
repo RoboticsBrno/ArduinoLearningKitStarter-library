59abe418d641ed99a891684fb2d7ccd495e380a2
Bedřich Said <BES@GRIFF.local>
2019-04-19 22:43:14 +0200

    Added Tomas Roj's examples and modified to work with both ESP32 and Arduino.

---
+++ examples/pwmLed/pwmLed.ino
@@ -0,0 +1,24 @@
+#import <ALKS.h>
+
+//We define all pins
+
+int potPin = A0;   
+int ledPin = 8;     
+
+int potProm = 0;    
+int ledProm = 0;   
+
+void setup(){
+}
+
+void loop(){
+  
+  potProm = analogRead(POT1);
+
+  ledProm = map(potProm, 0, 1023, 0, 255);
+ 
+  analogWrite(LED_BLUE, ledProm);
+
+  delay(2);
+}
+ 
