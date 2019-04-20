6e152da91cef53987bab45438c515ac9f29d62e8
Bedřich Said <BES@GRIFF.local>
2019-04-20 13:07:05 +0200

    Fixed pwmLed.ino to the ESP32 and Atmel Arduino compatibility.

--- examples/pwmLed/pwmLed.ino
+++ examples/pwmLed/pwmLed.ino
@@ -1,24 +1,35 @@
-#import <ALKS.h>
+#include <ALKS.h>
 
-//We define all pins
+int potPin = A0;
+int ledPin = 8;
 
-int potPin = A0;   
-int ledPin = 8;     
+int potVal = 0;
+int ledVal = 0;
 
-int potProm = 0;    
-int ledProm = 0;   
+#if defined(ESP32)
+const int freq = 2000;
+const int channel = 0;
+const int resolution = 8;
+#endif
 
 void setup(){
+#if defined(ESP32)
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(LED_GREEN, channel);
+#endif
 }
 
 void loop(){
-  
-  potProm = analogRead(POT1);
 
-  ledProm = map(potProm, 0, 1023, 0, 255);
- 
-  analogWrite(LED_BLUE, ledProm);
+  potVal = analogRead(POT1);
+
+#if defined(ESP32)
+  ledVal = map(potVal, 0, 4095, 0, 255);
+  ledcWrite(channel, ledVal);
+#else
+  ledVal = map(potVal, 0, 1023, 0, 255);
+  analogWrite(LED_BLUE, ledVal);
+#endif
 
   delay(2);
 }
- 
