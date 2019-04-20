c92c23c20fbfb3af49572b7874bf4b3915a80f11
Tomáš Roj <42382743+TomasRoj@users.noreply.github.com>
2019-04-20 09:17:39 +0200

    Update pwmLed.ino

--- examples/pwmLed/pwmLed.ino
+++ examples/pwmLed/pwmLed.ino
@@ -1,49 +1,26 @@
 #import <ALKS.h>
-
+ 
 //We define all pins
-
+ 
 int potPin = A0;   
-
+int ledPin = 8;     
+ 
 int potProm = 0;    
 int ledProm = 0;   
-
-const int freq = 5000; 
-const int ledChannel = 0; 
-const int resolution = 8;
-
-const int ledPin = 16;  // 16 corresponds to GPIO16
-
+ 
 void setup(){
-  ledcSetup(ledChannel, freq, resolution);
-  
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(ledPin, ledChannel);
 }
-
+ 
 void loop(){
   
-  #if defined(ESP32)
-    ledcSetup(ledChannel, freq, resolution);
-    
-    // attach the channel to the GPIO to be controlled
-    ledcAttachPin(ledPin, ledChannel);
-  
-    potProm = analogRead(POT1);
-
-    ledProm = map(potProm, 0, 1023, 0, 255);
-
-    analogWrite(LED_BLUE, ledProm);
-
-    delay(2);
-  #endif
-
-    potProm = analogRead(POT1);
-
-    ledProm = map(potProm, 0, 1023, 0, 255);
-
-    analogWrite(LED_BLUE, ledProm);
-
-    delay(2);
-  #endif
+  potProm = analogRead(POT1);
+ 
+  ledProm = map(potProm, 0, 1023, 0, 255);
+ 
+  analogWrite(LED_BLUE, ledProm);
+ 
+  delay(2);
 }
+ 
+
  
