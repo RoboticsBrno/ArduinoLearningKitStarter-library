15b546fd0386aadc2f8b3371ae6457f76006c282
CornyjK <cleverline1mc@gmail.com>
2019-04-20 16:10:39 +0200

    Modify example to proper state

--- examples/ledFade/ledFade.ino
+++ examples/ledFade/ledFade.ino
@@ -1,22 +1,31 @@
-// Not actual example, for CI purposes only
+/*
+  ALKS faded LED example
 
-#include <Arduino.h>
+  Example with fading LED.
+
+  Source: esp32 examples 
+  Modified by Kryštof Černý (@CornyjK) @RoboticsBrno
+*/
+
 #include <ALKS.h>
 
 int brightStep = 1;
 int brightness = 0;
 
-void setup() {
-  setupAll();
+void setup()
+{
+    setupAll();
 }
 
-void loop() {
-  brightness += brightStep;
-  if ( brightness == 0 || brightness == 255 ) {
-    brightStep = -brightStep;
-  }
+void loop()
+{
+    brightness += brightStep;
+    if (brightness == 0 || brightness == 255)
+    {
+        brightStep = -brightStep;
+    }
 
-  analogWrite(L_RGB_R, brightness);
+    analogWrite(L_RGB_R, brightness);
 
-  delay(10);
+    delay(10);
 }
